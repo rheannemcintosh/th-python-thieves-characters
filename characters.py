a4dc31cd7ad82226971a64d1be61bccc5bbfce74
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-20 23:32:53 +0100

    Create a character class

--- characters.py
+++ characters.py
@@ -1,17 +1,22 @@
 import random
 
-class Thief:
-    sneaky = True
+class Character:
 
-    def __init__(self, name, sneaky=True, **kwargs):
+    def __init__(self, name, **kwargs):
         self.name = name
-        self.sneaky = sneaky
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+class Thief(Character):
+    sneaky = True
+
+    def __init__(self, name, sneaky=True **kwargs):
+        super().__init__(name, **kwargs)
+        self.sneaky = sneaky
+
     def pickpocket(self):
-            return self.sneaky and bool(random.randint(0, 1))
+        return self.sneaky and bool(random.randint(0, 1))
 
     def hide(self, light_level):
         return self.sneaky and light_level < 10
